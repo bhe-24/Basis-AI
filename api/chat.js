d80deef933465aa9608f06f16edfadfd7203e99c
bhe-24 <ragamliterasi@gmail.com>
2026-02-22 08:20:08 +0700

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -15,7 +15,7 @@ export default async function handler(req, res) {
 
   try {
     const genAI = new GoogleGenerativeAI(apiKey);
-    const model = genAI.getGenerativeModel({ model: "gemma2-9b-it" });
+    const model = genAI.getGenerativeModel({ model: "gemini-3-flash-preview" });
 
     // Kita tangkap 'action' dan 'payload' dari frontend
     const { action, payload } = req.body;
